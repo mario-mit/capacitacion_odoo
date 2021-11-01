094ac4c13bda055f7e4693672362f29bb60c8eea
mario-mit <odoo@ampd2.odoo.com>
2021-11-01 23:59:03 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy' ,
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description':"""
+        Academy Module to manage training:
+        - Courses
+        - Sessions
+        - Attendees
+    """.
+    
+    'author': 'M',
+    
+    'webside': 'https://m.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+        
+    ]
+}
