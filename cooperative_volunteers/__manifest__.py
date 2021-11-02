c3b134efe64999a0b0cc33f5580ed299710d8446
mario-mit <odoo@ampd2.odoo.com>
2021-11-02 15:39:12 +0000

    [ADD] Cooperative Volunteers

---
+++ cooperative_volunteers/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Cooperative Volunteers",
+    
+    'summary': "Cooperative Volunteers app to manage sales",
+    
+    'description': """
+        Cooperative Volunteers app to help to manage:
+        - products
+        - sales
+    """,
+    
+    'author': "M",
+    
+    'website': "https://m.com",
+    
+    'version': "0.1",
+    
+    'category': "Volunteers",
+    
+    'depends':['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+        
+    ]
+}
