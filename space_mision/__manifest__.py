0c5c6c2abbbd9f6c8c2b457326ed109f8ca70a92
mario-mit <odoo@ampd2.odoo.com>
2021-11-02 14:55:14 +0000

    [ADD] Journey to the Moon

---
+++ space_mision/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "space_mision",
+    
+    'summary': """Space travel app to journey to the moon""",
+    
+    'description': """
+        Space travel app to manage:
+        - space ship
+        - crew
+    """,
+    
+    'author': "M",
+    
+    'webside': "https://m.com",
+    
+    'category': "Space",
+    
+    'version': "0.1",
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ]
+}
